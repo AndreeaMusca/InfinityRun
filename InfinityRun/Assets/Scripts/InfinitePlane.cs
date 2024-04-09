53d0e86c71755fcb9aee54f9ce09f015cb95ef0f
roommate6 <floginstefan@gmail.com>
2024-04-09 15:43:15 +0300

    +infinity plance script improved +bigger plane in z axis

--- InfinityRun/Assets/Scripts/InfinitePlane.cs
+++ InfinityRun/Assets/Scripts/InfinitePlane.cs
@@ -2,17 +2,9 @@ using UnityEngine;
 
 public class InfinitePlane : MonoBehaviour
 {
+	[SerializeField] private Transform _PlayerTransform;
 	void Update()
 	{
-		float snappedZ = Snap((int)Camera.main.transform.position.z);
-
-		transform.position = new Vector3(transform.position.x, transform.position.y, snappedZ);
-	}
-
-	int Snap(int value)
-	{
-		value /= 10;
-		value *= 10;
-		return value;
+		transform.position = new Vector3(transform.position.x, transform.position.y, _PlayerTransform.position.z);
 	}
 }
